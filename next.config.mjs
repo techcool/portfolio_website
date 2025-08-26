f64a14d195c4feeaf170b48510553c993407fccf
techcool <dev.assists@gmail.com>
2025-08-26 19:36:45 +0530

    add modified next config file and packag.json iles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.html$/,
+        loader: 'raw-loader',
+      });
+    }
+
+    return config;
+  }
+
+};
 
 export default nextConfig;
