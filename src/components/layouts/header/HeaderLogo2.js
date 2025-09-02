a2a936277e2209d3a3a15984a1b15fa5ceb155be
techcool <dev.assists@gmail.com>
2025-09-02 11:54:58 +0530

    add HeaderLogo2.js

--- src/components/layouts/header/HeaderLogo2.js
+++ src/components/layouts/header/HeaderLogo2.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from "next/image";
 // import logo from '../../../public/assets/imgs/logo.png'
-import logo from "../../../public/assets/imgs/black-logo.png"
-import whiteLogo from '../../../public/assets/imgs/white-logo.png'
+import logo from "@public/assets/imgs/black-logo.png"
+import whiteLogo from '@public/assets/imgs/white-logo.png'
 
 
 function HeaderLogo2() {
