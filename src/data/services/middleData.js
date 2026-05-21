456c72adc859bf4fe55b8e2e8bb5c89a0092a003
techcool <48747691+techcool@users.noreply.github.com>
2026-05-21 17:04:29 +0530

    updating middleData.js and topData.js in master branch

--- src/data/services/middleData.js
+++ src/data/services/middleData.js
@@ -1,70 +1,168 @@
-import webServicesTopPic from "@public/assets/imgs/thumb/dev-1.jpg"
-import trendImg from "@public/assets/imgs/thumb/service-detail.png"
-import expertImg from "@public/assets/imgs/service/6.jpg"
-import faqImg from "@public/assets/imgs/thumb/faq.jpg"
+import {
+  cmsTopImg,
+  cmsTrendsImg,
+  cmsExpertImg,
+  cmsFaqImg,
+} from "./serviceImages";
+
 export const middleData = [
-    {
-        id:1,
-        parent:"web-development",
-        slug:"cms",
-        categoryName:"CMS",
-        pageTitle:"Smart CMS Solutions for Effortless Website Management",
-        description:`<p>At Uniterrene Websoft, we empower businesses to take control of their websites with smart, scalable, and user-friendly CMS solutions. Our mission is to ensure that content management is a pain-free experience without jeopardizing the performance, security and design flexibility that the company currently needs. Whether it is a tailor-tailored CMS you require or you would rather use the services of such web platforms as WordPress, Joomla, or Drupal, our developers are sure that the process of controlling your online presence will become self-intuitive and effective.</p>`,
-        topImg:webServicesTopPic,
-        underStandingCMSHeading:"Understanding CMS: The Core of Modern Websites",
-        latestTrend:`<p>A Content Management System (CMS) is a software that enables users to create, edit and manage the content on a website without any technical skills. It bridges the gap between technology and creativity, which allows a seamless update and regular branding. By using a CMS, companies will have the ability to post blogs, update pages and control media instantly and all using one dashboard.</p>`,
-        trendImg:trendImg,
-        topFeatureHeading:"Top Features and Benefits That Make CMS Essential",
-        whyChooseContent:`<p>An effective CMS boosts group productivity and teamwork. Its capabilities make your business day-to-day use straightforward and your site is running well.</p>`,
-        topFeatureListHead:"Some of the key benefits include:",
-        TopFeatureList:[
-            {head:"Ease of Use:", list:"No experience in coding to update or create content."},
-            {head:"Customization:", list:"Custom design features where you can make your website move."},
-            {head:"SEO-Friendly Structure:", list:"Inbuilt applications that are helping in enhanced presence online."},
-            {head:"Security and Permissions:", list:"Role based access helps provide data security and control."},
-            {head:"Scalability:", list:"Scale your business with a growing functionality of your site."},
-            {head:"Cost Efficiency:", list:"Less reliance on developers to make changes on a regular basis."},
-        ],
-        behindTheSceneHeading:"Behind the Scenes: How a CMS Powers Your Website",
-        behindTheSceneContent:`<p>A CMS works in that the design and the content are decoupled. This framework is capable of enabling the user to edit or update content without affecting the entire layout. </p><p>Plugins, themes, and the third-party tools can be incorporated by developers, which enables easy updates and improvements. This type of modular system is easier to maintain and optimize websites without downtime. It is a flexible base that can change according to your business needs and maintain performance levels.</p>`,
-        expertImg:expertImg,
-        experTeamHeading:"Our Expert Team: Building CMS That Work for You",
-        expertTeamContent:"Our CMS developers at Uniterrene Websoft have a mixture of technical and creative strategy. It begins with an insight into your business requirements, followed by either a selection of the appropriate platform or a bespoke CMS to suit your workflow.",
-        epertTeamListHead:"Our process involves:",
-        expertTeamList:[
-            {
-                content:"Strategic planning and requirement analysis."
-            },
-            {
-                content:"UI/UX design that complements your brand."
-            },
-            {
-                content:"Development and integration of tailored CMS features."
-            },
-            {
-                content:"Quality testing and user training for seamless adoption."
-            }
-            
-        ],
-        faqImg:faqImg,
-        faqs:[
-            {
-                title:"What makes Uniterrene Websoft’s CMS solutions different?",
-                content:"Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
-                open: true,
-            },
-            {
-                title:"Can I update my website without technical skills?",
-                content:"Yes. Our CMS interfaces are simple and intuitive, allowing you to manage, edit, or upload content with ease.",
-                 open: false,
-            },
-            {
-                title:"Which platforms do you use for CMS development?",
-                content:"Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
-                open: false,
-            },
-        ],
-       
-        
-    }
-]
+  {
+    id: 1,
+    parent: "web-development",
+    slug: "cms",
+    categoryName: "CMS",
+    pageTitle: "Smart CMS Solutions for Effortless Website Management",
+    description: `<p>At Uniterrene Websoft, we empower businesses to take control of their websites with smart, scalable, and user-friendly CMS solutions. Our mission is to ensure that content management is a pain-free experience without jeopardizing the performance, security and design flexibility that the company currently needs. Whether it is a tailor-tailored CMS you require or you would rather use the services of such web platforms as WordPress, Joomla, or Drupal, our developers are sure that the process of controlling your online presence will become self-intuitive and effective.</p>`,
+    topImg: cmsTopImg,
+    underStandingCMSHeading: "Understanding CMS: The Core of Modern Websites",
+    latestTrend: `<p>A Content Management System (CMS) is a software that enables users to create, edit and manage the content on a website without any technical skills. It bridges the gap between technology and creativity, which allows a seamless update and regular branding. By using a CMS, companies will have the ability to post blogs, update pages and control media instantly and all using one dashboard.</p>`,
+    trendImg: cmsTrendsImg,
+    topFeatureHeading: "Top Features and Benefits That Make CMS Essential",
+    whyChooseContent: `<p>An effective CMS boosts group productivity and teamwork. Its capabilities make your business day-to-day use straightforward and your site is running well.</p>`,
+    topFeatureListHead: "Some of the key benefits include:",
+    TopFeatureList: [
+      {
+        head: "Ease of Use:",
+        list: "No experience in coding to update or create content.",
+      },
+      {
+        head: "Customization:",
+        list: "Custom design features where you can make your website move.",
+      },
+      {
+        head: "SEO-Friendly Structure:",
+        list: "Inbuilt applications that are helping in enhanced presence online.",
+      },
+      {
+        head: "Security and Permissions:",
+        list: "Role based access helps provide data security and control.",
+      },
+      {
+        head: "Scalability:",
+        list: "Scale your business with a growing functionality of your site.",
+      },
+      {
+        head: "Cost Efficiency:",
+        list: "Less reliance on developers to make changes on a regular basis.",
+      },
+    ],
+    behindTheSceneHeading: "Behind the Scenes: How a CMS Powers Your Website",
+    behindTheSceneContent: `<p>A CMS works in that the design and the content are decoupled. This framework is capable of enabling the user to edit or update content without affecting the entire layout. </p><p>Plugins, themes, and the third-party tools can be incorporated by developers, which enables easy updates and improvements. This type of modular system is easier to maintain and optimize websites without downtime. It is a flexible base that can change according to your business needs and maintain performance levels.</p>`,
+    expertImg: cmsExpertImg,
+    experTeamHeading: "Our Expert Team: Building CMS That Work for You",
+    expertTeamContent:
+      "Our CMS developers at Uniterrene Websoft have a mixture of technical and creative strategy. It begins with an insight into your business requirements, followed by either a selection of the appropriate platform or a bespoke CMS to suit your workflow.",
+    epertTeamListHead: "Our process involves:",
+    expertTeamList: [
+      {
+        content: "Strategic planning and requirement analysis.",
+      },
+      {
+        content: "UI/UX design that complements your brand.",
+      },
+      {
+        content: "Development and integration of tailored CMS features.",
+      },
+      {
+        content: "Quality testing and user training for seamless adoption.",
+      },
+    ],
+    faqImg: cmsFaqImg,
+    faqs: [
+      {
+        title: "What makes Uniterrene Websoft’s CMS solutions different?",
+        content:
+          "Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
+        open: true,
+      },
+      {
+        title: "Can I update my website without technical skills?",
+        content:
+          "Yes. Our CMS interfaces are simple and intuitive, allowing you to manage, edit, or upload content with ease.",
+        open: false,
+      },
+      {
+        title: "Which platforms do you use for CMS development?",
+        content:
+          "Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
+        open: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    parent: "web-development",
+    slug: "ecommerce-development",
+    categoryName: "Ecommerce Development",
+    pageTitle: "Get Your Smart E-commerce Development To Elevate Your Business",
+    description: `<p>At Uniterrene Websoft, we transform visionary business ideas into powerful, scalable, and high-performing eCommerce platforms. Our goal is to create digital stores that not only attract but also convert visitors into loyal customers. Whether you need a multi-vendor marketplace, a custom online boutique, or a global retail solution, our team ensures your platform is fast, secure, and optimized for growth.</p><p>We render innovativeness, software and commerce to build eCommerce solutions that make the shopping experiences easier but impact the business the most. Each phase is usability-oriented, conversion-oriented and based on strong backend architecture, between the idea and the final product. Our profound knowledge of modern technologies will enable you to create an online shop that looks eye-catching, is rather sound technologically, and is prepared to face the future.</p>`,
+    topImg: cmsTopImg,
+    underStandingCMSHeading: "Shaping the Future of Online Retail Today",
+    latestTrend: `<p>The concept of eCommerce is changing at a high pace and customers desire quicker and smarter, tailored shopping process. Increasingly popular AI-based recommendations, voice assisted checkouts, and secure check out gateways have changed the buying and selling of people and brands. Your online store in this digital revolution should be dynamic, data-driven, and customer-driven.</p><p>At Uniterrene Websoft, we incorporate these trends in all the projects we develop. Through smart data processing and reactive design and inventory management, we ensure that your platform is on top of the trend. The development strategy will be scalable and flexible, thus your eCommerce will expand as your business objectives and market dynamics expand.</p>`,
+    trendImg: cmsTrendsImg,
+    topFeatureHeading:
+      "Why Choose Uniterrene Websoft as Your eCommerce Partner",
+    whyChooseContent: `<p>We are strong in accuracy, innovation and profound knowledge in digital commerce. We do not simply create websites to sell anything but develop growth-oriented ecosystems that turn visitors into customers and customers into brand evangelists. Any project that we do gets optimized in terms of speed, security and scalability.</p>`,
+    topFeatureListHead: "We ensure every project delivers:",
+    TopFeatureList: [
+      {
+        head: "Intelligent, conversion-driven user experience.",
+        list: "",
+      },
+      {
+        head: "Mobile-friendly responsive design.",
+        list: "",
+      },
+      {
+        head: "API and smooth compatibility with new technologies.",
+        list: "",
+      },
+      {
+        head: "Consistent performance through constant testing and quality control.",
+        list: "",
+      },
+    ],
+    behindTheSceneHeading: "",
+    behindTheSceneContent: ``,
+    expertImg: cmsExpertImg,
+    experTeamHeading: "Our Expert Team: Building CMS That Work for You",
+    expertTeamContent:
+      "Our CMS developers at Uniterrene Websoft have a mixture of technical and creative strategy. It begins with an insight into your business requirements, followed by either a selection of the appropriate platform or a bespoke CMS to suit your workflow.",
+    epertTeamListHead: "Our process involves:",
+    expertTeamList: [
+      {
+        content: "Strategic planning and requirement analysis.",
+      },
+      {
+        content: "UI/UX design that complements your brand.",
+      },
+      {
+        content: "Development and integration of tailored CMS features.",
+      },
+      {
+        content: "Quality testing and user training for seamless adoption.",
+      },
+    ],
+    faqImg: cmsFaqImg,
+    faqs: [
+      {
+        title: "What makes Uniterrene Websoft’s CMS solutions different?",
+        content:
+          "Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
+        open: true,
+      },
+      {
+        title: "Can I update my website without technical skills?",
+        content:
+          "Yes. Our CMS interfaces are simple and intuitive, allowing you to manage, edit, or upload content with ease.",
+        open: false,
+      },
+      {
+        title: "Which platforms do you use for CMS development?",
+        content:
+          "Our CMS solutions exist with the focus on your brand objectives and will be flexible, easy to use and scalable in the long term.",
+        open: false,
+      },
+    ],
+  },
+];
